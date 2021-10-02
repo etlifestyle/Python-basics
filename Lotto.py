71cf6c0452053f3e39f496e7033722e0c826d8f4
etlifestyle <91784233+etlifestyle@users.noreply.github.com>
2021-10-02 06:05:45 -0600

    Update Lotto.py

--- Lotto.py
+++ Lotto.py
@@ -1,5 +1,5 @@
 
-lotto_numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+lotto_numbers=[1,4,6,9]
 lotto_num=input('Lotto Number:')
 if lotto_numbers.__contains__(int(lotto_num)) :
     print('You win ')
